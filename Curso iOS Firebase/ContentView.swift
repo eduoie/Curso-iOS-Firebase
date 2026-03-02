003384f6c2ee6b93844b540e10f2acf2b43f6c11
Eduardo <eduardo@microsoft.com>
2026-03-02 10:07:08 +0100

    Initial Commit

---
+++ Curso iOS Firebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Curso iOS Firebase
+//
+//  Created by Equipo 2 on 2/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Curso iOS Firebase/Curso_iOS_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_iOS_FirebaseApp.swift
+//  Curso iOS Firebase
+//
+//  Created by Equipo 2 on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_iOS_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
