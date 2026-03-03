670b5ba3019de83ba32aa4c3acd2e6451cf50944
Eduardo <eduardo@microsoft.com>
2026-03-03 09:58:08 +0100

    Vista de gastos y vista de añadir gastos

---
+++ Curso iOS Firebase/modelos/Gasto.swift
@@ -0,0 +1,24 @@
+//
+//  Gasto.swift
+//  Curso iOS Firebase
+//
+//  Created by Equipo 2 on 2/3/26.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Gasto: Identifiable, Codable {
+    // Firestore rellena DocumentID automáticamente
+    @DocumentID var id: String?
+    
+    var titulo: String
+    var importe: Double
+    var fecha: Date
+    
+    var idUsuario: String // Necesario para saber de qué usuario es el gasto
+    
+    enum CodingKeys: String, CodingKey {
+        case id, titulo, importe, fecha, idUsuario
+    }
+}
